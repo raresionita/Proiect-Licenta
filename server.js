91527e0c975a51f3a03066a06331d7bfe3eabfe9
IonitaRares <rares.ionita98@e-uvt.ro>
2020-05-01 02:24:47 +0300

    updates to deploy to heroku final6

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/licenta'));
+// app.use(express.static(__dirname + '/dist/licenta'));
+app.use(express.static("./src"))
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/licenta/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/licenta/index.html'));
+// });
 
 
 
