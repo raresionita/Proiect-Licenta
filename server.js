e74151ffad54a4ccd6171f1e38f0e0458bb8196e
IonitaRares <rares.ionita98@e-uvt.ro>
2020-05-01 02:08:37 +0300

    updates to deploy to heroku final3

--- server.js
+++ server.js
@@ -5,17 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-//app.use(express.static(__dirname + '/../src'));
+app.use(express.static(__dirname + './src'));
 
-//app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
 
-//res.sendFile(path.join(__dirname+'/../src/index.html'));
-//});
-
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'src', 'index.html');
-  res.sendFile(index);
+res.sendFile(path.join(__dirname+'./index.html'));
 });
+
+
 
 // Start the app by listening on the default Heroku portt
 app.listen(process.env.PORT || 8080);
