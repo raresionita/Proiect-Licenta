13d71627c2b27b6c21e3d1eec6fcdacda2fdc9fe
IonitaRares <rares.ionita98@e-uvt.ro>
2020-05-01 01:43:08 +0300

    updates to deploy to heroku8

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src/index.html'));
+app.use(express.static(__dirname + '/dist/src'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku portt
